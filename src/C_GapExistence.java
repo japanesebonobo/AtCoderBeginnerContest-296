5315daec26691e394f3374ad8b747e9db4570ff0
japanesebonobo <tomi119119@gmail.com>
2023-04-01 23:42:15 +0900

    fix C

--- src/C_GapExistence.java
+++ src/C_GapExistence.java
@@ -5,37 +5,19 @@ public class C_GapExistence {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         long X = scanner.nextLong();
-        long[] A = new long[N];
+        TreeSet<Long> A = new TreeSet<>();
         for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt();
+            A.add(scanner.nextLong());
         }
         scanner.close();
-        
-        for (int i = 0; i < N; i++) {
-            long target = A[i] - X;
-            if (search(A, target)) {
-                System.out.print("Yes");
-                System.exit(0);;
+
+        for (Iterator<Long> iterator = A.iterator(); iterator.hasNext();) {
+            long target = iterator.next() - X;
+            if (A.contains(target)) {
+                System.out.println("Yes");
+                System.exit(0);
             } 
         }
-
         System.out.println("No");
     }
-
-    public static Boolean search(long[] data, long target) {
-		Boolean ret = false;
-		long left = 0;
-		long right = data.length - 1;
-		while (left <= right) {
-			long mid = (left + right) / 2;
-			if (data[(int) mid] == target) {
-				return true;
-			} else if (target < data[(int) mid]) {
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-			}
-		}
-		return ret;
-	}
 }
